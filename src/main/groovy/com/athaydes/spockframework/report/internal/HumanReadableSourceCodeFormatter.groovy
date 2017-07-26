1a9687e267d7bb0b24a7131602f7f1b74f3d5d11
Tomasz Łoś <tloszabno@gmail.com>
2017-07-26 18:33:27 +0200

    Some fixes in src human readable formatter

--- src/main/groovy/com/athaydes/spockframework/report/internal/HumanReadableSourceCodeFormatter.groovy
+++ src/main/groovy/com/athaydes/spockframework/report/internal/HumanReadableSourceCodeFormatter.groovy
@@ -5,22 +5,43 @@ package com.athaydes.spockframework.report.internal
  */
 class HumanReadableSourceCodeFormatter implements com.athaydes.spockframework.report.internal.SourceCodeFormatter {
 
+    private static final String REGEXP_FOR_CAMEL_CASE = "(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])";
+
     @Override
     List<String> format(List<String> blocks) {
-        blocks.collect { it-> formatBlock(it)}
+        blocks.collect { it-> formatBlock(it) }
     }
 
     String formatBlock(String block) {
-        block = block.replace('(','')
-        block = block.replace(')','')
-        block = block.replace('"','')
-        block = block.replace('\'','')
-        block = block.replace('.',' ')
-        block = block.replace(':',' ')
-        block = block.replace('==','is equal to')
-        block = block.replace('_',' ')
-
+        block = replaceChars(block)
+        block = slitCamelCaseNamesAndToLowerFromSecond(block)
         block
+    }
+
+
+    private String replaceChars(String block) {
+        block = block.replace('(', '')
+        block = block.replace(')', '')
+        block = block.replace('"', '')
+        block = block.replace('\'', '')
+        block = block.replace('.', ' ')
+        block = block.replace(':', ' ')
+        block = block.replace('==', 'is equal to')
+        block = block.replace('_', ' ')
+        block = block.trim().replaceAll('^def$', '')
+        block = block.replace(' = ', ' is ')
+        block
+    }
+
+    private String slitCamelCaseNamesAndToLowerFromSecond(String block) {
+        String [] split = block.split(REGEXP_FOR_CAMEL_CASE)
+        if( split.length > 1 ){
+           //FIXME: write in groovy way
+            for (int i = 1; i < split.length; i++) {
+                split[i] = split[i].toLowerCase()
+            }
+        }
+        split.join(" ")
     }
 
 }
